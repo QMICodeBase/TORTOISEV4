b3f1fe2daca658e6c34672acaf96ccee6c9cd045
eurotomania <okan.irfanoglu@gmail.com>
2025-07-01 16:38:01 -0400

    modified:   TORTOISEV4/CMakeLists.txt 	new file:   src/tools/CombineTransformations/combine_transformations.cxx 	new file:   src/tools/CombineTransformations/combine_transformations_with_output_name.cxx 	new file:   src/tools/CombineTransformations/combine_transformations_with_reference.cxx

---
+++ src/tools/CombineTransformations/combine_transformations_with_output_name.cxx
@@ -0,0 +1,226 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+#include "itkDiffusionTensor3D.h"
+#include "vnl/algo/vnl_symmetric_eigensystem.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkComposeImageFilter.h"
+#include "itkExtractImageFilter.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkAffineTransform.h"
+#include "itkTransformFileReader.h"
+#include "itkTransformFileWriter.h"
+#include "itkCompositeTransform.h"
+#include "itkDisplacementFieldTransform.h"
+
+
+
+    typedef double RealType;        
+    typedef  vnl_matrix_fixed< RealType, 3, 3 > InternalMatrixType;
+    typedef itk::AffineTransform<RealType,3> AffineTransformType;
+    
+    typedef itk::Image<itk::Vector<RealType,3>,  3>          DisplacementFieldType;
+    typedef itk::DisplacementFieldTransform<RealType, 3>    DisplacementFieldTransformType;
+    
+    typedef itk::CompositeTransform<RealType, 3>                CompositeTransformType;
+    typedef itk::Transform<RealType,3, 3>             TransformType;
+    typedef AffineTransformType::Superclass                                   MatrixOffsetTransformBaseType;
+    
+           
+    
+AffineTransformType::Pointer CollapseLinearTransforms( CompositeTransformType * compositeTransform )
+{
+    typename AffineTransformType::Pointer totalTransform = AffineTransformType::New();
+    
+  
+    for( unsigned int n = 0; n < compositeTransform->GetNumberOfTransforms(); n++ )
+    {
+        TransformType::Pointer transform = compositeTransform->GetNthTransform( n );
+        AffineTransformType::Pointer nthTransform = AffineTransformType::New();
+
+        MatrixOffsetTransformBaseType::ConstPointer matrixOffsetTransform = dynamic_cast<MatrixOffsetTransformBaseType * const>( transform.GetPointer() );
+        nthTransform->SetMatrix( matrixOffsetTransform->GetMatrix() );
+        nthTransform->SetOffset( matrixOffsetTransform->GetOffset() );
+
+        totalTransform->Compose( nthTransform, true );
+    }
+    return totalTransform;
+}
+
+
+    
+
+int main( int argc , char * argv[] )
+{
+    if(argc<5)    
+    {
+        std::cout<<"Usage:   CombineTransformationsWithOutputName  -out full_path_to_output_trans_name -trans full_path_to_trans1 full_path_to_trans2 ..............   full_path_to_trans_n "<<std::endl;
+        return 0;
+    }
+    
+    int out_tag_pos=-1;
+    int trans_tag_pos=-1;
+    int last_trans_pos=-1;
+    
+    
+    for(int i=0;i<argc;i++)
+    {
+        if(string(argv[i])==string("-out"))
+            out_tag_pos=i;
+        
+        if(string(argv[i])==string("-trans"))
+            trans_tag_pos=i;
+    }
+    
+    if(out_tag_pos==-1)
+    {
+        std::cout<<"-out tag not present in the arguments..."<<std::endl;
+        std::cout<<"Usage:   CombineTransformationsWithOutputName  -out full_path_to_output_trans_name -trans full_path_to_trans1 full_path_to_trans2 ..............   full_path_to_trans_n "<<std::endl;
+        return 0;        
+    }
+    
+    if(trans_tag_pos==-1)
+    {
+        std::cout<<"-trans tag not present in the arguments..."<<std::endl;
+        std::cout<<"Usage:   CombineTransformationsWithOutputName  -out full_path_to_output_trans_name -trans full_path_to_trans1 full_path_to_trans2 ..............   full_path_to_trans_n "<<std::endl;
+        return 0;        
+    }
+    
+    if(out_tag_pos<trans_tag_pos)
+    {
+        last_trans_pos=argc-1;        
+    }
+    else
+    {
+        last_trans_pos= out_tag_pos-1;
+    }
+    
+    
+    
+      
+    std::string currdir;
+    std::string nm(argv[out_tag_pos+1]);
+    
+            
+    int mypos= nm.rfind("/");
+    if(mypos ==-1)
+        currdir= std::string("./");
+    else   
+       currdir= nm.substr(0,mypos+1);
+    
+    
+    CompositeTransformType::Pointer composite_trans= CompositeTransformType::New();
+    DisplacementFieldType::Pointer ref_disp_field=nullptr;
+    
+    bool all_affine=1;    
+    for(int i=trans_tag_pos+1;i<=last_trans_pos;i++) 
+    {
+        std::string filename(argv[i]);
+        
+        
+        std::string::size_type idx=filename.rfind('.');
+        if(idx != std::string::npos)
+        {
+            std::string extension = filename.substr(idx+1);
+            if(extension == string("nii"))
+            {
+                all_affine=0;   
+                                      
+                typedef itk::ImageFileReader<DisplacementFieldType> FieldReaderType;
+                typename FieldReaderType::Pointer mreader=FieldReaderType::New();
+                mreader->SetFileName(filename);
+                mreader->Update();        
+                DisplacementFieldTransformType::Pointer disp_trans= DisplacementFieldTransformType::New();
+                disp_trans->SetDisplacementField(mreader->GetOutput());
+                ref_disp_field=mreader->GetOutput();
+                composite_trans->AddTransform(disp_trans);
+            }
+            else
+            {
+                if(extension == string("txt"))
+                {
+                    itk::TransformFileReader::Pointer reader;
+                    reader = itk::TransformFileReader::New();
+                    reader->SetFileName(filename);
+                    reader->Update();
+                    typedef itk::TransformFileReader::TransformListType * TransformListType;
+                    TransformListType transforms = reader->GetTransformList();   
+                    itk::TransformFileReader::TransformListType::const_iterator it = transforms->begin(); 
+                    AffineTransformType::Pointer affine_trans = static_cast<AffineTransformType*>((*it).GetPointer());
+                    composite_trans->AddTransform(affine_trans);
+                }
+                else
+                {
+                    std::cout<<"Transformation " << i-trans_tag_pos << " does not contain .nii or .txt.. Exiting!!"<<std::endl;
+                    exit(0);                                    
+                }                
+            }
+        }
+        else
+        {
+            std::cout<<"Transformation " << i- trans_tag_pos<< " does not contain .nii or .txt.. Exiting!!"<<std::endl;
+            exit(0);
+        }        
+    }
+
+    composite_trans->FlattenTransformQueue();
+    composite_trans->SetOnlyMostRecentTransformToOptimizeOn();
+    
+    if(all_affine)
+    {
+        AffineTransformType::Pointer combined_affine= CollapseLinearTransforms(composite_trans );
+                
+        string tr_name= currdir + string("combined_affine.txt");    
+        itk::TransformFileWriter::Pointer trwriter = itk::TransformFileWriter::New();
+        trwriter->SetInput(combined_affine);
+        trwriter->SetFileName(tr_name);
+        trwriter->Update();
+    }
+    else
+    {
+        DisplacementFieldType::Pointer combined_displacement_field= DisplacementFieldType::New();
+        combined_displacement_field->SetRegions(ref_disp_field->GetLargestPossibleRegion());
+        combined_displacement_field->Allocate();
+        combined_displacement_field->SetSpacing(ref_disp_field->GetSpacing());
+        combined_displacement_field->SetOrigin(ref_disp_field->GetOrigin());
+        combined_displacement_field->SetDirection(ref_disp_field->GetDirection());  
+                
+        DisplacementFieldType::SizeType imsize= ref_disp_field->GetLargestPossibleRegion().GetSize();
+        
+    
+        for( int k=0; k<(int)imsize[2];k++)
+        {
+            DisplacementFieldType::IndexType index;
+            index[2]=k;
+        
+            for(unsigned int j=0; j<imsize[1];j++)
+            {
+                index[1]=j;
+                for(unsigned int i=0; i<imsize[0];i++)
+                {                     
+                    index[0]=i;
+                                                  
+                    DisplacementFieldType::PointType pf;
+                    combined_displacement_field->TransformIndexToPhysicalPoint(index,pf);
+                    DisplacementFieldType::PointType pmt = composite_trans->TransformPoint(pf);
+                    DisplacementFieldType::PixelType vec = pmt-pf;
+                    combined_displacement_field->SetPixel(index,vec);
+                }
+            }
+        }
+        
+        
+        typedef itk::ImageFileWriter<DisplacementFieldType> WriterType;
+        WriterType::Pointer writer= WriterType::New();
+        string name =nm;
+        writer->SetFileName(name);
+        writer->SetInput(combined_displacement_field);
+        writer->Update();
+    }
+}
---
+++ src/tools/CombineTransformations/combine_transformations_with_reference.cxx
@@ -0,0 +1,197 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+#include "itkDiffusionTensor3D.h"
+#include "vnl/algo/vnl_symmetric_eigensystem.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkComposeImageFilter.h"
+#include "itkExtractImageFilter.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkAffineTransform.h"
+#include "itkTransformFileReader.h"
+#include "itkTransformFileWriter.h"
+#include "itkCompositeTransform.h"
+#include "itkDisplacementFieldTransform.h"
+
+
+
+    typedef double RealType;        
+    typedef  vnl_matrix_fixed< RealType, 3, 3 > InternalMatrixType;
+    typedef itk::AffineTransform<RealType,3> AffineTransformType;
+    
+    typedef itk::Image<itk::Vector<RealType,3>,  3>          DisplacementFieldType;
+    typedef itk::DisplacementFieldTransform<RealType, 3>    DisplacementFieldTransformType;
+    
+    typedef itk::CompositeTransform<RealType, 3>                CompositeTransformType;
+    typedef itk::Transform<RealType,3, 3>             TransformType;
+    typedef AffineTransformType::Superclass                                   MatrixOffsetTransformBaseType;
+    
+           
+    
+AffineTransformType::Pointer CollapseLinearTransforms( CompositeTransformType * compositeTransform )
+{
+    typename AffineTransformType::Pointer totalTransform = AffineTransformType::New();
+    
+  
+    for( unsigned int n = 0; n < compositeTransform->GetNumberOfTransforms(); n++ )
+    {
+        TransformType::Pointer transform = compositeTransform->GetNthTransform( n );
+        AffineTransformType::Pointer nthTransform = AffineTransformType::New();
+
+        MatrixOffsetTransformBaseType::ConstPointer matrixOffsetTransform = dynamic_cast<MatrixOffsetTransformBaseType * const>( transform.GetPointer() );
+        nthTransform->SetMatrix( matrixOffsetTransform->GetMatrix() );
+        nthTransform->SetOffset( matrixOffsetTransform->GetOffset() );
+
+        totalTransform->Compose( nthTransform, true );
+    }
+    return totalTransform;
+}
+
+
+    
+
+int main( int argc , char * argv[] )
+{
+    if(argc<4)    
+    {
+        std::cout<<"Usage:   CombineTransformationsWithReference full_path_to_Refence_image_with_desired_spacing   full_path_to_trans1 full_path_to_trans2 ..............   full_path_to_trans_n "<<std::endl;
+        std::cout<<"The output name will be combined_displacement.nii in the reference image folder"<<std::endl;
+        return 0;
+    }
+    
+    
+      
+    std::string currdir;
+    std::string nm(argv[1]);
+    
+            
+    int mypos= nm.rfind("/");
+    if(mypos ==-1)
+        currdir= std::string("./");
+    else   
+       currdir= nm.substr(0,mypos+1);
+    
+    
+    CompositeTransformType::Pointer composite_trans= CompositeTransformType::New();
+    DisplacementFieldType::Pointer ref_disp_field=nullptr;
+    
+    bool all_affine=1;    
+    for(int i=2;i<argc;i++) 
+    {
+        std::string filename(argv[i]);
+        std::string::size_type idx=filename.rfind('.');
+        if(idx != std::string::npos)
+        {
+            std::string extension = filename.substr(idx+1);
+            if(extension == string("nii"))
+            {
+                all_affine=0;   
+                                      
+                typedef itk::ImageFileReader<DisplacementFieldType> FieldReaderType;
+                typename FieldReaderType::Pointer mreader=FieldReaderType::New();
+                mreader->SetFileName(filename);
+                mreader->Update();        
+                DisplacementFieldTransformType::Pointer disp_trans= DisplacementFieldTransformType::New();
+                disp_trans->SetDisplacementField(mreader->GetOutput());
+                composite_trans->AddTransform(disp_trans);
+            }
+            else
+            {
+                if(extension == string("txt"))
+                {
+                    itk::TransformFileReader::Pointer reader;
+                    reader = itk::TransformFileReader::New();
+                    reader->SetFileName(filename);
+                    reader->Update();
+                    typedef itk::TransformFileReader::TransformListType * TransformListType;
+                    TransformListType transforms = reader->GetTransformList();   
+                    itk::TransformFileReader::TransformListType::const_iterator it = transforms->begin(); 
+                    AffineTransformType::Pointer affine_trans = static_cast<AffineTransformType*>((*it).GetPointer());
+                    composite_trans->AddTransform(affine_trans);
+                }
+                else
+                {
+                    std::cout<<"Transformation " << i-1 << " does not contain .nii or .txt.. Exiting!!"<<std::endl;
+                    exit(0);                                    
+                }                
+            }
+        }
+        else
+        {
+            std::cout<<"Transformation " << i-1 << " does not contain .nii or .txt.. Exiting!!"<<std::endl;
+            exit(0);
+        }        
+    }
+
+    composite_trans->FlattenTransformQueue();
+    composite_trans->SetOnlyMostRecentTransformToOptimizeOn();
+    
+    if(all_affine)
+    {
+        AffineTransformType::Pointer combined_affine= CollapseLinearTransforms(composite_trans );
+                
+        string tr_name= currdir + string("combined_affine.txt");    
+        itk::TransformFileWriter::Pointer trwriter = itk::TransformFileWriter::New();
+        trwriter->SetInput(combined_affine);
+        trwriter->SetFileName(tr_name);
+        trwriter->Update();
+    }
+    else
+    {
+        typedef itk::ImageFileReader<DisplacementFieldType> FieldReaderType;
+        
+        DisplacementFieldType::PixelType zero;
+        zero.Fill(0);
+        
+        typename FieldReaderType::Pointer mreader=FieldReaderType::New();
+        mreader->SetFileName(argv[1]);
+        mreader->Update();    
+        ref_disp_field=mreader->GetOutput();
+        
+        DisplacementFieldType::Pointer combined_displacement_field= DisplacementFieldType::New();
+        combined_displacement_field->SetRegions(ref_disp_field->GetLargestPossibleRegion());
+        combined_displacement_field->Allocate();
+        combined_displacement_field->SetSpacing(ref_disp_field->GetSpacing());
+        combined_displacement_field->SetOrigin(ref_disp_field->GetOrigin());
+        combined_displacement_field->SetDirection(ref_disp_field->GetDirection());  
+        combined_displacement_field->FillBuffer(zero);
+                
+        DisplacementFieldType::SizeType imsize= ref_disp_field->GetLargestPossibleRegion().GetSize();
+        
+    
+        for( int k=0; k<(int)imsize[2];k++)
+        {
+            DisplacementFieldType::IndexType index;
+            index[2]=k;
+        
+            for(unsigned int j=0; j<imsize[1];j++)
+            {
+                index[1]=j;
+                for(unsigned int i=0; i<imsize[0];i++)
+                {                     
+                    index[0]=i;
+                                                  
+                    DisplacementFieldType::PointType pf;
+                    combined_displacement_field->TransformIndexToPhysicalPoint(index,pf);
+                    DisplacementFieldType::PointType pmt = composite_trans->TransformPoint(pf);
+                    DisplacementFieldType::PixelType vec = pmt-pf;
+                    combined_displacement_field->SetPixel(index,vec);
+                }
+            }
+        }
+        
+        
+        typedef itk::ImageFileWriter<DisplacementFieldType> WriterType;
+        WriterType::Pointer writer= WriterType::New();
+        string name =currdir + string("combined_displacement.nii");    
+        writer->SetFileName(name);
+        writer->SetInput(combined_displacement_field);
+        writer->Update();
+    }
+}
